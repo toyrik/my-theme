cbbc5bdf71aff8eb3e3d32fafa1c39ed9e4aa09c
toyrik <toyrik@gmail.com>
2022-04-21 09:41:46 +0300

    Параметры функций

--- single.php
+++ single.php
@@ -3,7 +3,7 @@
     <div class="container">
         <div class="row">
         <?php the_post() ?>
-        <?php the_date() ?>
+        <?php the_date('j F Y') ?>
         <h3><?php the_title() ?></h3>
         <div><?php the_content() ?></div>
         </div>        
